0f5d61e737dd6d3600fbe8d690786452e3263bb7
MarharytaLepushynska <m.lepushynska@ukma.edu.ua>
2026-06-08 19:44:58 +0300

    add tests for tcp

--- Practice1/src/test/java/ServerTCPTest.java
+++ Practice1/src/test/java/ServerTCPTest.java
@@ -1,3 +1,4 @@
+import Db.StorageDb;
 import Db.StorageService;
 import Network.ServerTCP;
 import Network.StoreClientTCP;
@@ -5,49 +6,56 @@ import Tools.Decoder;
 import Tools.Message;
 import Tools.Product;
 import Tools.Storage;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerTCPTest {
     private static StorageService service = new StorageService();
+    private StorageDb db = new StorageDb("jdbc:mysql://localhost:3306/store_db", "root", "root");
     private Decoder decoder = new Decoder();
+    private ServerTCP server;
 
     @Test
     void shouldCreateProduct() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
+        ArrayBlockingQueue<String> queue2 = new ArrayBlockingQueue<>(50);
         new Thread(new StoreClientTCP(queue)).start();
-        new Thread(new StoreClientTCP(queue)).start();
+        new Thread(new StoreClientTCP(queue2)).start();
         Thread.sleep(1000);
-
 
         queue.put("2 рис 5 70 крупи");
+        queue2.put("2 сочевиця 40 50 крупи");
 
         Thread.sleep(4000);
         assertEquals(5, service.getAmountByName("рис"));
-        server.stop();
+
+        int id1 = service.getIdByName("рис");
+        Product p1 = db.getById(id1);
+        assertEquals("рис", p1.getName());
+        assertEquals(5, p1.getAmount());
+        assertEquals(70.0, p1.getPrice());
+        assertEquals("крупи", p1.getCategory());
+
+        int id2 = service.getIdByName("сочевиця");
+        Product p2 = db.getById(id2);
+        assertEquals("сочевиця", p2.getName());
+        assertEquals(40, p2.getAmount());
+        assertEquals(50.0, p2.getPrice());
+        assertEquals("крупи", p2.getCategory());
     }
 
     @Test
     void shouldGetProductById() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
         StoreClientTCP client = new StoreClientTCP(queue);
         new Thread(client).start();
@@ -67,16 +75,10 @@ class ServerTCPTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        server.stop();
     }
 
     @Test
     void shouldUpdateProduct() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
         StoreClientTCP client = new StoreClientTCP(queue);
         new Thread(client).start();
@@ -97,15 +99,10 @@ class ServerTCPTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        server.stop();
     }
 
     @Test
     void shouldDeleteProduct() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
         StoreClientTCP client = new StoreClientTCP(queue);
         new Thread(client).start();
@@ -129,31 +126,22 @@ class ServerTCPTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        server.stop();
     }
 
     @Test
     void deleteAllProducts() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
         StoreClientTCP client = new StoreClientTCP(queue);
         new Thread(client).start();
 
         Thread.sleep(1000);
 
-
         queue.put("5");
-
         queue.put("6");
-
 
         Thread.sleep(4000);
 
-        client.getResponses().poll();
+        client.getResponses().take();
         byte[] res = client.getResponses().take();
         try {
             Message message = decoder.decode(res);
@@ -161,16 +149,10 @@ class ServerTCPTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        server.stop();
     }
 
     @Test
     void shouldFilterProducts() throws InterruptedException{
-        ServerTCP server = new ServerTCP(1, 1);
-        server.start();
-        Thread.sleep(1000);
-
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
         StoreClientTCP client = new StoreClientTCP(queue);
         new Thread(client).start();
@@ -178,7 +160,6 @@ class ServerTCPTest {
         Thread.sleep(1000);
 
         queue.put("6 price_from=12 price_to=30 category=солодке");
-
 
         Thread.sleep(4000);
 
@@ -190,25 +171,91 @@ class ServerTCPTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
 
+    @Test
+    void shouldAddPagination() throws InterruptedException{
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
+        StoreClientTCP client = new StoreClientTCP(queue);
+        new Thread(client).start();
+
+        Thread.sleep(1000);
+
+        queue.put("6 page_number=2 page_size=3");
+
+
+        Thread.sleep(4000);
+
+        byte[] res = client.getResponses().take();
+        try {
+            Message message = decoder.decode(res);
+            String text = message.getMessage();
+
+            long count = Stream.of(text.split("\n"))
+                    .filter(line -> !line.isEmpty())
+                    .count();
+
+            assertTrue(count <= 3);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void shouldProcessMultipleCommands() throws InterruptedException{
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(50);
+        StoreClientTCP client = new StoreClientTCP(queue);
+        new Thread(client).start();
+
+        Thread.sleep(1000);
+
+        int id = service.getIdByName("баклажан");
+
+        queue.put("3 id=" + id + " name=булгур category=крупи");
+        queue.put("2 макарони 100 50 крупи");
+        queue.put("3 id=" + id + " name=мак price=10 category=крупи");
+
+        Thread.sleep(4000);
+
+        Product p1 = db.getById(id);
+        assertEquals("мак", p1.getName());
+        assertEquals(10, (double) p1.getPrice());
+        assertEquals("крупи", p1.getCategory());
+
+        int id2 = service.getIdByName("макарони");
+        Product p2 = db.getById(id2);
+        assertEquals(p2.getName(), "макарони");
+        assertEquals(p2.getAmount(), 100);
+        assertEquals(p2.getPrice(), 50.0);
+        assertEquals(p2.getCategory(), "крупи");
+    }
+
+    @BeforeEach
+    void startServer() throws InterruptedException {
+        server = new ServerTCP(1, 1);
+        server.start();
+        Thread.sleep(1000);
+    }
+
+    @AfterEach
+    void stopServer(){
         server.stop();
     }
 
     @BeforeAll
     static void setUp() {
-        Product p1 = new Product("монстр", 10, 24.5, "напої");
-        Product p2 = new Product("баклажан", 20, 4.5, "овочі");
-        Product p3 = new Product("огірок", 20, 4.5, "овочі");
-        Product p4 = new Product("печиво", 10, 9.0, "солодке");
-        Product p5 = new Product("желе", 15, 10.0, "солодке");
-        Product p6 = new Product("бджілка", 20, 14.5, "солодке");
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("монстр", 10, 24.5, "напої"));
+        products.add(new Product("баклажан", 20, 4.5, "овочі"));
+        products.add(new Product("огірок", 20, 4.5, "овочі"));
+        products.add(new Product("печиво", 10, 9.0, "солодке"));
+        products.add(new Product("желе", 15, 10.0, "солодке"));
+        products.add(new Product("бджілка", 20, 14.5, "солодке"));
+        products.add(new Product("зефір", 30, 50.99, "солодке"));
 
-        service.create(p1);
-        service.create(p2);
-        service.create(p3);
-        service.create(p4);
-        service.create(p5);
-        service.create(p6);
+        for(Product product: products) {
+            service.create(product);
+        }
     }
 
     @AfterAll
